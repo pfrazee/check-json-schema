58938a11bd9a3804379eb6701da26bb8ceaf88bc
Paul Frazee <pfrazee@gmail.com>
2018-11-07 11:53:40 -0600

    Skip not-useful files

--- bin.js
+++ bin.js
@@ -5,6 +5,23 @@ const path = require('path')
 
 var ps = []
 for (let schemaFilePath of process.argv.slice(2)) {
+  // skip non json
+  if (!schemaFilePath.endsWith('.json')) {
+    console.log('skipping', schemaFilePath)
+    continue
+  }
+
+  // skip common json
+  if (schemaFilePath.endsWith('package.json')) {
+    console.log('skipping package.json')
+    continue
+  }
+  if (schemaFilePath.endsWith('package-lock.json')) {
+    console.log('skipping package-lock.json')
+    continue
+  }
+
+  // validate
   ps.push(checker.validate(schemaFilePath).catch(console.error))
 }
 Promise.all(ps).then(success => {
